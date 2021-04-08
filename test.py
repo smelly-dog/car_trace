da6a47567db215a593e0ddf78e406526319ae2c0
1313113 <741937263@qq.com>
2021-04-08 22:35:14 +0800

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+import torch, string
+from Model.model import POI
+
+if __name__ == '__main__':
+    pois = POI(114.120514, 22.810388)
+    projectionMatrix = [[x['location']] for x in pois]
+    for i in range(len(projectionMatrix)):
+        temp = projectionMatrix[i][0]
+        a1, a2 = temp.split(',')
+        longitude, latitude = float(a1), float(a2)
+        projectionMatrix[i] = [longitude, latitude]
+    print(projectionMatrix)
